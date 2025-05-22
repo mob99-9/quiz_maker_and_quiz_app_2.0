b14fe938bd9b868df725ff85fce448a468feadb3
mob99-9 <jamesorozco20050@gmail.com>
2025-05-22 20:55:58 +0800

    made adding to csv file an object

---
+++ quiz_maker.py
@@ -0,0 +1,31 @@
+import csv
+
+file_name = "data_questions.csv"
+
+class SaveQuestion:
+    def add_to_file(self, user_inputs):
+        with open(file_name, "a", newline = "") as file:
+            writer = csv.writer(file)
+            writer.writerows(user_inputs)
+
+question = []
+
+def question_maker():
+    question_set = []
+    while True:
+        user_question = input("Please enter a question (type exit if none): ")
+        if user_question.lower() == "exit":
+            exit()
+        else:
+            question_set.append(user_question)
+            for choice in range(0,4):
+                user_choice = input(f"Please enter choice no. {choice + 1}: ")
+                question_set.append(user_choice)
+            user_answer = input("Please enter the corret answer (A, B, C, D): ")
+            question_set.append(user_answer)
+            question.append(question_set)
+            add_question = SaveQuestion()
+            add_question.add_to_file(question)
+
+if __name__ == '__main__':
+    question_maker()
